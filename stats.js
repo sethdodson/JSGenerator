1fd353c8bb4b68594ca4630dc3af452a87aae2fe
Seth Dodson <seth_dodson@hotmail.com>
2012-10-22 20:40:26 -0400

    Added stats

---
+++ stats.js
@@ -0,0 +1,14 @@
+var App = App || {};
+
+App.MeetsRequirements = function (abilities, requirements) {
+	var meetsRequirement = function (ability, requirement) {
+		return ability >= requirement.Minimum && ability <= requirement.Maximum;
+	},
+		strengthOK = meetsRequirement(abilities.Strength, requirements.Strength),
+		dexterityOK = meetsRequirement(abilities.Dexterity, requirements.Dexterity),
+		constitutionOK = meetsRequirement(abilities.Constitution, requirements.Constitution),
+		intelligenceOK = meetsRequirement(abilities.Intelligence, requirements.Intelligence),
+		wisdomOK = meetsRequirement(abilities.Wisdom, requirements.Wisdom),
+		charismaOK = meetsRequirement(abilities.Charisma, requirements.Charisma);
+		return strengthOK && dexterityOK && constitutionOK && intelligenceOK && wisdomOK && charismaOK;
+};
